e0baf2e3991321fe20b3fb2564e728e0600f787a
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-14 23:23:48 +0100

    change sink image

--- src/components/image.jsx
+++ src/components/image.jsx
@@ -5,7 +5,7 @@ import Img from "gatsby-image"
 const Image = () => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "sink.jpg" }) {
+      placeholderImage: file(relativePath: { eq: "sink.png" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
