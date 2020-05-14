a205e1d0981984d3f12f84fb9d3ecfdee3c446a5
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-14 20:17:35 +0100

    add footer component

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,35 @@
+import React from "react"
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <h3>Stay in Touch!</h3>
+      <div className="sink-details">
+        <ul className="sink-details__list">
+          <li title="twitter">
+            <a href="https://twitter.com/sinkopuba">TW</a>
+          </li>
+          <li title="linkedin">
+            <a href="https://www.linkedin.com/in/sinkopere-opuba">LN</a>
+          </li>
+          <li title="RSS Feed">
+            <a href="/#RSS">RS</a>
+          </li>
+          <li title="Email">
+            <a href="mailto:osinkopere@yahoo.com">EM</a>
+          </li>
+        </ul>
+      </div>
+      <div className="copyright-box">
+        <p>
+          {" "}
+          © {new Date().getFullYear()}, Built with 🤎 by
+          {` `}
+          Sink-Opuba
+        </p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer
