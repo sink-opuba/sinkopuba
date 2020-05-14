a18f3c022e99415b398d90241c0c04684efe58ea
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-14 10:27:31 +0100

    add gatsby-transformer-remark

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,6 +56,8 @@ module.exports = {
             linkImagesToOriginal: false,
           },
         },
+      ],
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
