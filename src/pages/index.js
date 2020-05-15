1822fac34ad73ea4fa1438fd53a6b1f9fa114367
sink-opuba <39055986+sink-opuba@users.noreply.github.com>
2020-05-15 21:11:40 +0100

    query allposts with graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,33 +1,51 @@
 import React from "react"
-// import { Link } from "gatsby"
+import { useStaticQuery, graphql } from "gatsby"
 
 import Layout from "../components/layout.jsx"
 import Image from "../components/image"
 import SEO from "../components/seo"
 import BlogList from "../components/BlogList/BlogList"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Sinkopere Opuba" />
-    <div className="index-container">
-      <section className="sink-section">
-        <div className="sink-section__heading">
-          <h1>
-            {" "}
-            Hi, I'm Sinkopere Opuba, a software Developer with a bias toward
-            JavaScript and its ecosystem. Thanks for stopping by👏
-          </h1>
-        </div>
-        <div className="sink-section__img">
-          <Image />
-        </div>
-      </section>
-      <section className="blog-section">
-        <h3>Articles</h3>
-        <BlogList />
-      </section>
-    </div>
-  </Layout>
-)
+const IndexPage = () => {
+  const data = useStaticQuery(graphql`
+    query postQuery {
+      allMarkdownRemark(sort: { order: DESC, fields: [frontmatter___date] }) {
+        edges {
+          node {
+            frontmatter {
+              title
+              date(formatString: "MMM Do, YYYY")
+              path
+            }
+            excerpt(pruneLength: 200)
+          }
+        }
+      }
+    }
+  `)
+  return (
+    <Layout>
+      <SEO title="Sinkopere Opuba" />
+      <div className="index-container">
+        <section className="sink-section">
+          <div className="sink-section__heading">
+            <h1>
+              {" "}
+              Hi, I'm Sinkopere Opuba, a software Developer with a bias toward
+              JavaScript and its ecosystem. Thanks for stopping by👏
+            </h1>
+          </div>
+          <div className="sink-section__img">
+            <Image />
+          </div>
+        </section>
+        <section className="blog-section">
+          <h3>Articles</h3>
+          <BlogList posts={data} />
+        </section>
+      </div>
+    </Layout>
+  )
+}
 
 export default IndexPage
